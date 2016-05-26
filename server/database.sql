dc91d01ca745840f4a98a455a0ef190a5cc8da11
Veena Sridhar <v.sridhar91@gmail.com>
2016-05-26 14:08:35 -0700

    Refactor database file into PostgreSQL

--- server/database.sql
+++ server/database.sql
@@ -1,76 +1,33 @@
--- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
-
--- ---
--- Table 'Farm'
--- 
--- ---
+-- Creates the Farm table --
 
 DROP TABLE IF EXISTS `Farm`;
 		
 CREATE TABLE `Farm` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL PRIMARY KEY,
   `farmName` VARCHAR(255) UNIQUE DEFAULT NULL,
   `farmLocation` VARCHAR(255) NULL DEFAULT NULL,
-  `farmPhone` VARCHAR(12) UNIQUE DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `farmPhone` VARCHAR(10) UNIQUE DEFAULT NULL
 );
 
--- ---
--- Table 'products'
--- 
--- ---
+-- Creates the product table --
 
 DROP TABLE IF EXISTS `products`;
 		
 CREATE TABLE `products` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `productName` VARCHAR(255) UNIQUE DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL PRIMARY KEY ,
+  `productName` VARCHAR(255) UNIQUE DEFAULT NULL
 );
 
--- ---
--- Table 'Post'
--- 
--- ---
+
+-- Creates the Post table --
 
 DROP TABLE IF EXISTS `Post`;
 		
 CREATE TABLE `Post` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_Farm` INTEGER NULL DEFAULT NULL,
-  `id_products` INTEGER NULL DEFAULT NULL,
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL PRIMARY KEY,
+  `id_Farm` integer REFERENCES Farm,
+  `id_products` integer REFERENCES products,
   `pricePerPound` INTEGER(6) NULL DEFAULT NULL,
-  `poundsAvailable` INTEGER(6) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `poundsAvailable` INTEGER(6) NULL DEFAULT NULL
 );
 
--- ---
--- Foreign Keys 
--- ---
-
-ALTER TABLE `Post` ADD FOREIGN KEY (id_Farm) REFERENCES `Farm` (`id`);
-ALTER TABLE `Post` ADD FOREIGN KEY (id_products) REFERENCES `products` (`id`);
-
--- ---
--- Table Properties
--- ---
-
--- ALTER TABLE `Farm` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Post` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `Farm` (`id`,`farmName`,`farmLocation`,`farmPhone`) VALUES
--- ('','','','');
--- INSERT INTO `products` (`id`,`productName`) VALUES
--- ('','');
--- INSERT INTO `Post` (`id`,`id_Farm`,`id_products`,`pricePerPound`,`poundsAvailable`) VALUES
--- ('','','','','');
