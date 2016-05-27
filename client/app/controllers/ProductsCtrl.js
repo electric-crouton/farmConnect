e2c01df30181084c6f9bbb8d2f8d49635b18488b
Tegan Duong <teganduong@gmail.com>
2016-05-27 16:46:09 -0700

    Remove unnecessary code

--- client/app/controllers/ProductsCtrl.js
+++ client/app/controllers/ProductsCtrl.js
@@ -15,25 +15,4 @@ angular.module('farmConnect.products', [])
     .then(function(products) {
       $scope.items.push(products);
     });
-
-
-  $scope.done = function(todo) {
-    var indexOf = $scope.todos.indexOf(todo);
-    if (indexOf !== -1) {
-      $scope.todos.splice(indexOf, 1);
-    }
-    fx
-  };
-
-  $scope.add = function(e) {
-    if (e.which && e.which === 13) {
-      $scope.todos.push($scope.newTodo);
-      $scope.newTodo = '';
-    }
-  };
-
-  $scope.taskAdd1 = function() {
-    $scope.todos.push($scope.taskInput1);
-    $scope.taskInput1 = '';
-  };
 });
