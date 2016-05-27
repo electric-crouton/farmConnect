14a2970f8255d1d9b79beeb5c242d7fbcf2f57b2
Ben Rachbach <brachbach@gmail.com>
2016-05-27 15:37:18 -0700

    Refactor connect.js into an exportable module and make query using productCollection

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,17 +2,29 @@ var pg = require('pg');
 
 var clientLink = 'postgres://localhost:5432/farmdata';
 
-pg.connect(clientLink, function (err, client, done) {
-  if (err) {
-    console.error('error!', err);
-    process.exit(1);
+// pg.connect(clientLink, function (err, client, done) {
+//   if (err) {
+//     console.error('error!', err);
+//     process.exit(1);
+//   }
+// });
+
+module.exports = {
+  query: function(text, values, cb) {
+    pg.connect(clientLink, function(err, client, done) {
+      client.query(text, values, function(err, result) {
+        done();
+        cb(err, result);
+      });
+    });
   }
-  client.query('SELECT * FROM farm', function (err, result) {
-    if (err) {
-      console.error('error!', err);
-    } else {
-      console.log(result);
-    }
-    done();
-  });
-});
+};
+
+// client.query('SELECT * FROM farm', function (err, result) {
+//     if (err) {
+//       console.error('error!', err);
+//     } else {
+//       console.log(result);
+//     }
+//     done();
+// });
