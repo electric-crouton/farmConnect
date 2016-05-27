ceea9e29b37d980d9569a51274d12a70903a9ab2
Tegan Duong <teganduong@gmail.com>
2016-05-26 20:02:05 -0700

    Implement addProduct method in Products factory

--- client/app/controllers/AddProductsCtrl.js
+++ client/app/controllers/AddProductsCtrl.js
@@ -1,7 +1,11 @@
 angular.module('farmConnect.addProducts', [])
 
-.controller('AddProductsCtrl', function($scope) {
+.controller('AddProductsCtrl', function($scope, Products) {
+
+  $scope.product = {};
   
-  $scope.addProduct = function () {};
+  $scope.addProduct = function () {
+    Products.addProduct($scope.product);
+  };
 
 });
--- client/app/services.js
+++ client/app/services.js
@@ -1,1 +1,16 @@
 angular.module ('farmConnect.services', [])
+
+.factory('Products', function($http) {
+  var Products = {};
+
+  Products.addProduct = function(product) {
+    return $http.post('/api/products', product)
+    .then(function(resp) {
+      console.log('Product successfully added!');
+    }, function(err) {
+      console.error('Error in adding product!', err);
+    });
+  };
+
+  return Products;
+});
