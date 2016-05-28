5d26ac9bb62762938b9c7711823f7ad3f491dfa4
Ben Rachbach <brachbach@gmail.com>
2016-05-27 18:25:06 -0700

    Add console.log for server initialization

--- server/server.js
+++ server/server.js
@@ -4,4 +4,9 @@ var app = express();
 require('./config/middlewares/initialization.js')(app, express);
 require('./config/routes.js')(app);
 
+
+console.log('Server listening on port 1337');
+
 app.listen(1337);
+
+
