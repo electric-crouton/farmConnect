8fab676f20adc360088383ca5beaeacb5e4a452c
Ben Rachbach <brachbach@gmail.com>
2016-05-29 23:17:49 -0700

    Save posts in database, creating new produce and farm entries if neccessary

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,5 +2,7 @@ const productsController = require('../products/productsCollection.js');
 
 module.exports = (app) => {
   app.get('/api/products', productsController.getProducts);
-  app.post('/api/products', productsController.addFarm);
+  app.post('/api/products', productsController.addFarmIfNecessary);
+  app.post('/api/products', productsController.addProduceIfNecessary);
+  app.post('/api/products', productsController.addProduct);
 };
--- server/products/productsCollection.js
+++ server/products/productsCollection.js
@@ -10,11 +10,59 @@ var getProducts = module.exports.getProducts = (req, res) => {  // note that thi
   });
 };
   
-var addFarm = module.exports.addFarm = (req, res) => {
+var addFarmIfNecessary = module.exports.addFarmIfNecessary = (req, res, next) => {
+  product = req.body;
+  connection.query(`SELECT EXISTS (SELECT * FROM farm WHERE name = '${product.farmName}')`, (err, result) => {
+    if (!result.rows[0].exists) {
+      connection.query(
+        `INSERT INTO farm (name, location, phone)\
+        VALUES ('${product.farmName}', '${product.farmLocation}', '${product.farmPhone}')`, 
+
+        (err, result) => {
+          if (err) {
+            console.error('error:', err);
+          } else {
+            next();
+          }
+        }
+      );
+    } else {
+      next();
+    }
+  });
+};
+
+var addProduceIfNecessary = module.exports.addProduceIfNecessary = (req, res, next) => {
+  product = req.body;
+  connection.query(`SELECT EXISTS (SELECT * FROM produce WHERE name = '${product.productName}')`, (err, result) => {
+    if (!result.rows[0].exists) {
+      connection.query(
+        `INSERT INTO produce (name)\
+        VALUES ('${product.productName}')`, 
+
+        (err, result) => {
+          if (err) {
+            console.error('error:', err);
+          } else {
+            next();
+          }
+        }
+      );
+    } else {
+      next();
+    }
+  });
+};
+
+var addProduct = module.exports.addProduct = (req, res) => {
   product = req.body;
   connection.query(
-    `INSERT INTO farm (name, location, phone)\
-    VALUES ('${product.farmName}', '${product.farmLocation}', '${product.farmPhone}')`, 
+    `INSERT INTO post\
+    (farm_id, produce_id, pricePerPound, amountAvailable)\
+    VALUES ((SELECT id FROM farm WHERE name = '${product.farmName}'),\
+    (SELECT id FROM produce WHERE name = '${product.productName}'),\
+    ${product.pricePerPound},\
+    ${product.poundsAvailable})`,
 
     (err, result) => {
       if (err) {
@@ -26,59 +74,3 @@ var addFarm = module.exports.addFarm = (req, res) => {
     }
   );
 };
-
-var addProduce = module.exports.addProduce = (req, res) => {
-  product = req.body;
-  connection.query(
-    `INSERT INTO produce (name)\
-    VALUES ('${product.productName}')`, 
-
-    (err, result) => {
-      if (err) {
-        console.error('error:', err);
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(200);
-      }
-    }
-  );
-};
-// var addProduct = module.expo (req, res) => {
-//     product = req.body;
-//     // console.log(req.body);
-//     connection.query(`SELECT EXISTS (SELECT * FROM farm WHERE name = '${product.farmName}')`, (err, result) => {
-//       console.log('exists:', result.rows[0].exists);
-//       if (!result.rows[0].exists) {
-//         connection.query(
-//           `INSERT INTO farm (name, location, phone)\
-//           VALUES ('${product.farmName}', '${product.farmLocation}', '${product.farmPhone}')`,          
-        
-//         (err, result) => {
-//           if (err) {
-//             console.error('error:', err);
-//             res.sendStatus(500);
-//           } else {
-//             res.sendStatus(200);
-//           }
-//       res.send(result);
-//     });
-//     connection.query(
-//       `INSERT INTO post\
-//       (farm_id, produce_id, pricePerPound, poundsAvailable)\  //note that this was the var name that was wrong in the database
-//       VALUES ((SELECT id FROM farm WHERE name = '${product.farmName}'),\
-//       (SELECT id FROM produce WHERE name = '${product.productName}'),\
-//       ${product.pricePerPound},\
-//       ${product.poundsAvailable})`,
-
-//       (err, result) => {
-//         if (err) {
-//           console.error('error:', err);
-//           res.sendStatus(500);
-//         } else {
-//           res.sendStatus(200);
-//         }
-//       }
-
-//     );
-//   }
-// };
