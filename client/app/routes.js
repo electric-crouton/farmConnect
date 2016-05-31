977daa47cc7761574deadbea77afa6cb3b4b5d9b
Tegan Duong <teganduong@gmail.com>
2016-05-30 19:05:56 -0700

    Link correct templates to current page in navbar

--- client/app/routes.js
+++ client/app/routes.js
@@ -8,7 +8,7 @@ angular.module('farmConnect.routes', [])
       templateUrl: 'views/AddProductForm.html',
       controller: 'AddProductsCtrl'
     })
-    .state('productState', {
+    .state('products', {
       url: '/products',
       templateUrl: 'views/ProductsPage.html',
       controller: 'ProductsCtrl'
