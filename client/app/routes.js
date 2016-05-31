17e6ef4a4b74787d47f18d5cd7cf49b256c26b49
Tegan Duong <teganduong@gmail.com>
2016-05-30 20:11:41 -0700

    Add signin and signup routes and require signin for selling products

--- client/app/routes.js
+++ client/app/routes.js
@@ -3,17 +3,28 @@ angular.module('farmConnect.routes', [])
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-    .state('home', {
-      url: '/',
+    .state('addProduct', {
+      url: '/sell',
       templateUrl: 'views/AddProductForm.html',
-      controller: 'AddProductsCtrl'
+      controller: 'AddProductsCtrl',
+      authenticate: true
     })
     .state('products', {
       url: '/products',
       templateUrl: 'views/ProductsPage.html',
       controller: 'ProductsCtrl'
+    })
+    .state('signin', {
+      url: '/signin',
+      templateUrl: 'views/signin.html',
+      controller: 'AuthController'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'views/signup.html',
+      controller: 'AuthController'
     });
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/products');
   
 });
