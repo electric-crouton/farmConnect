3baebfad37b0480518d24a9137571e7558a45179
Tegan Duong <teganduong@gmail.com>
2016-05-30 23:12:19 -0700

    Create Auth factory and controller

---
+++ client/app/controllers/AuthCtrl.js
@@ -0,0 +1,9 @@
+angular.module('farmConnect.auth', [])
+
+.controller('AuthCtrl', function($scope) {
+  $scope.user = {};
+
+  $scope.signin = function() {};
+
+  $scope.signup = function() {};
+});
