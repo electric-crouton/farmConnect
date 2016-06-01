dfa77df5c482b6e2273198cabdb14eca52787b0d
Tegan Duong <teganduong@gmail.com>
2016-06-01 13:28:16 -0700

    Add comment in addProductCtrl explaining commented out lines of code

--- client/app/controllers/AddProductsCtrl.js
+++ client/app/controllers/AddProductsCtrl.js
@@ -7,6 +7,7 @@ angular.module('farmConnect.addProducts', [])
   
   $scope.addProduct = () => {
     Products.addProduct($scope.product);
+    // lines below are commented out bc they're not fully functional yet, need to load some ui-bootstrap stuff
     // .then(() => {
     //   $scope.product = {};
     //   $scope.alert = {
