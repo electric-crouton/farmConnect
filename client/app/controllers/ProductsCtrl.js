7453d239ed22b863a01560d2d4402505fbbb73af
Tegan Duong <teganduong@gmail.com>
2016-06-01 16:12:13 -0700

    Create shopping cart item in nav and implement addToCart

--- client/app/controllers/ProductsCtrl.js
+++ client/app/controllers/ProductsCtrl.js
@@ -1,15 +1,29 @@
 angular.module('farmConnect.products', [])
 
-.controller('ProductsCtrl', function($scope, Products) {
+.controller('ProductsCtrl', function($scope, Products, $rootScope) {
 
   $scope.items = [];
-
   $scope.search = '';
+  $rootScope.cart = [];
+  $rootScope.cartSummary = {
+    numOfItems: 0,
+    total: 0
+  };
 
   Products.getProducts()
     .then((products) => {
+      console.log('products in controller: ', products);
       products.forEach((product) => {
         $scope.items.push(product);
       });
     });
+
+  $scope.addToCart = (item) => {
+    if ($rootScope.cart.indexOf(item) === -1) {
+      $rootScope.cart.push(item);
+      $rootScope.cartSummary.numOfItems++;
+    } else {
+      console.log('Item already in cart!');
+    }
+  };
 });
