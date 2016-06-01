642ffe357ece47cb1368a9ea7bca206393755f3b
Veena Sridhar <v.sridhar91@gmail.com>
2016-05-31 18:27:06 -0700

    Change table names and add passport dependencies

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,7 +6,7 @@ CREATE DATABASE farmdata;
 
 -- Creates the farm table
 
-CREATE TABLE farm (
+CREATE TABLE farms (
   id SERIAL PRIMARY KEY,
   farm_name VARCHAR(50) UNIQUE,
   location VARCHAR(100),
@@ -15,17 +15,24 @@ CREATE TABLE farm (
 
 --Creates the produce table
 
-CREATE TABLE produce (
+CREATE TABLE products (
   id SERIAL PRIMARY KEY,
   product_name VARCHAR(100)
 );
 
 --Creates the post table
 
-CREATE TABLE post (
+CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
   farm_id integer REFERENCES farm (id),
   produce_id integer REFERENCES produce (id),
   price_per_pound decimal(10, 2),
   pounds_available integer
+);
+
+CREATE TABLE users (
+ id SERIAL PRIMARY KEY,
+ username VARCHAR(30) UNIQUE,
+ password VARCHAR(20) UNIQUE,
+ farmer BOOLEAN NOT NULL DEFAULT FALSE
 );
