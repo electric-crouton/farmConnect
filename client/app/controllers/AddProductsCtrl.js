5dabd7118ce5c94e3b75ab0ec6f8eb84bfa91ac8
Tegan Duong <teganduong@gmail.com>
2016-05-31 17:43:25 -0700

    Start on implementing alert for successful product submission

--- client/app/controllers/AddProductsCtrl.js
+++ client/app/controllers/AddProductsCtrl.js
@@ -3,9 +3,26 @@ angular.module('farmConnect.addProducts', [])
 .controller('AddProductsCtrl', function($scope, Products) {
 
   $scope.product = {};
+  $scope.alert = {};
   
   $scope.addProduct = () => {
     Products.addProduct($scope.product);
+    // .then(() => {
+    //   $scope.product = {};
+    //   $scope.alert = {
+    //     type: 'success', 
+    //     msg: 'Your product was successfully added!'
+    //   };
+    // })
+    // .error(() => {
+    //   $scope.alert = {
+    //     type: 'danger', 
+    //     msg: 'There was an error in adding your product! Please try again.'
+    //   };
+    // });
   };
+
+  // $scope.closeAlert = function() {
+  // };
 
 });
