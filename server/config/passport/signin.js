64c1aa6257a829ec72c1f56aca1b25603bc62788
Veena Sridhar <v.sridhar91@gmail.com>
2016-05-31 20:30:54 -0700

    Add minor changes to the signup and signin pages

--- server/config/passport/signin.js
+++ server/config/passport/signin.js
@@ -1,8 +1,16 @@
-const connection = require('../db/connection.js');
-var app = express();
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
 
 //to allow logging in
 
-app.get();
+router.get('/signin', function(req, res) {
+  console.log('request is:', req);
+  res.render('login', { user : req.user });
+});
+
+router.post('/signin', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
 
 
