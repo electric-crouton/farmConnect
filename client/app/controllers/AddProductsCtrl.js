1ea6f1cf13da16d128721430363c6460b672c032
Tegan Duong <teganduong@gmail.com>
2016-06-02 16:41:31 -0700

    Alert user when item is successfully added to products page

--- client/app/controllers/AddProductsCtrl.js
+++ client/app/controllers/AddProductsCtrl.js
@@ -6,24 +6,24 @@ angular.module('farmConnect.addProducts', [])
   $scope.alert = {};
   
   $scope.addProduct = () => {
-    Products.addProduct($scope.product);
-    // lines below are commented out bc they're not fully functional yet, need to load some ui-bootstrap stuff
-    // .then(() => {
-    //   $scope.product = {};
-    //   $scope.alert = {
-    //     type: 'success', 
-    //     msg: 'Your product was successfully added!'
-    //   };
-    // })
-    // .error(() => {
-    //   $scope.alert = {
-    //     type: 'danger', 
-    //     msg: 'There was an error in adding your product! Please try again.'
-    //   };
-    // });
+    Products.addProduct($scope.product)
+    .then(() => {
+      $scope.product = {};
+      $scope.alert = {
+        type: 'success', 
+        msg: 'Your product was successfully added!'
+      };
+    })
+    .catch(() => {
+      $scope.alert = {
+        type: 'danger', 
+        msg: 'There was an error in adding your product! Please try again.'
+      };
+    });
   };
 
-  // $scope.closeAlert = function() {
-  // };
+  $scope.closeAlert = () => {
+    $scope.alert = {};
+  };
 
 });
