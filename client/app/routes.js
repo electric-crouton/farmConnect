086ac792c0179e62779ec743d16c8ddc6a144d11
Nathanael Ligon <nathanaelligon@Nathanaels-Air.att.net>
2016-06-02 16:00:45 -0700

    Create 'about' page

--- client/app/routes.js
+++ client/app/routes.js
@@ -3,6 +3,11 @@ angular.module('farmConnect.routes', [])
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
+    .state('products', {
+      url: '/products',
+      templateUrl: 'views/ProductsPage.html',
+      controller: 'ProductsCtrl'
+    })
     .state('addProduct', {
       url: '/sell',
       templateUrl: 'views/addProductForm.html',
@@ -31,12 +36,12 @@ angular.module('farmConnect.routes', [])
     });
 
   $urlRouterProvider.otherwise('/products');
-  
+
 })
 
 .run(function($rootScope, $location, Auth) {
-  
-  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+
+  $rootScope.$on('$routeChangeStart', function(evt, next, current) {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/signin');
     }
