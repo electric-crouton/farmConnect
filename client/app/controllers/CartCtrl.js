ce670611675d6bc5f73a84726e441c63ea681f2b
Tegan Duong <teganduong@gmail.com>
2016-06-03 12:19:19 -0700

    Show cart summary table when there are items in cart

--- client/app/controllers/CartCtrl.js
+++ client/app/controllers/CartCtrl.js
@@ -1,5 +1,14 @@
 angular.module('farmConnect.cart', [])
 
-.controller('CartCtrl', function($scope, $rootScope) {
+.controller('CartCtrl', function($scope, $rootScope, ngCart) {
   console.log('$rootScope.cart: ', $rootScope.cart);
+  $rootScope.cart = $rootScope.cart || [];
+
+  ngCart.setTaxRate(9.0);
+  ngCart.setShipping(2.99); 
+
+  $scope.getTotalItems = () => {
+    return $rootScope.cart.length;
+  };
+
 });
