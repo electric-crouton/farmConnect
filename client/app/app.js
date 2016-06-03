ea633a7f84b73217dc1ad3d7fdac82d92bd65fec
Tegan Duong <teganduong@gmail.com>
2016-06-03 14:38:46 -0700

    Remove unused dependencies and update cart table

--- client/app/app.js
+++ client/app/app.js
@@ -6,9 +6,5 @@ angular.module('farmConnect', [
   'farmConnect.services',
   'farmConnect.auth',
   'farmConnect.cart',
-  'ngAnimate',
-  // 'ui.bootstrap',
-  'ui.bootstrap.tpls',
-  'ngMaterial',
-  'ngCart'
+  'ngAnimate'
 ]);
