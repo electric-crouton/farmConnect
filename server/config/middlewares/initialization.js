831e0e7cb64c013a8b1c9c4579da5b795c300913
Veena Sridhar <v.sridhar91@gmail.com>
2016-06-06 10:33:53 -0700

    Add comments to server side code

--- server/config/middlewares/initialization.js
+++ server/config/middlewares/initialization.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 
+
+//enable parsing of requests, and connects server to front end
 module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
