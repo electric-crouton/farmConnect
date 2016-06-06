405e7cd6494b1898764edb69708bb9048133523c
Ben Rachbach <brachbach@gmail.com>
2016-06-06 14:51:04 -0700

    Fix bugs to reconnect frontend and backend

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -34,9 +34,9 @@ var generateHash = function(password) {
 
 var insertIntoFarmsTable = function(req, res, token) {
   const farm = req.body;
+  console.log('farm:', farm);
   connection.query({
-      text: 'INSERT INTO farms (user_id, farm_name, location, phone)\
-      VALUES ((SELECT id FROM users WHERE email = $1), $2, $3, $4', 
+      text: 'INSERT INTO farms (user_id, farm_name, location, phone) VALUES ((SELECT id FROM users WHERE email = $1), $2, $3, $4)', 
       values: [farm.email, farm.farmName, farm.farmLocation, farm.farmPhone]
     },
 
@@ -56,19 +56,21 @@ var insertIntoFarmsTable = function(req, res, token) {
 
 var insertIntoUsersTable = function (req, res) {
   const user = req.body;
+  console.log ('user:', user);
   const hash = generateHash(user.password);
   req.body.password = hash;
 
   connection.query({
-      text: 'INSERT INTO users (email, password, farmer) VALUES ($1, $2) RETURNING id',
-      values: [user.email, hash, user.farmer]
+      text: 'INSERT INTO users (email, password, farmer) VALUES ($1, $2, $3) RETURNING id',
+      values: [user.email, hash, user.isFarmer] //change to farmer
     }, 
 
     (err, result) => {
       if (err) { console.log('error in inserting user into users table'); }
       user.id = result.rows[0].id;
       const token = jwt.sign({id: user.id}, 'JtEnMq9j0pNlQ0lXZhJEnm', {expiresIn: '2h'});  
-      if (user.farmer === 'true') {
+      if (user.isFarmer === 'true') {   //change to farmer
+        console.log('inserting farmer');
         insertIntoFarmsTable(req, res, token);
       } else {
         res.status(201).json({
--- server/products/productsCollection.js
+++ server/products/productsCollection.js
@@ -19,14 +19,14 @@ exports.getPosts = (req, res) => {
           poundsAvailable: datum.pounds_available
         };
       });
+      console.log('posts:', posts);
       res.send(posts);
     }
   });
 };
 
-//add the farm to the farms table if it wasn't already there
-//will be obsoleted once auth stuff is merged in 
-var addFarmIfNecessary = (req, res, callback1, callback2) => {
+//add the product to the products table if it wasn't already there
+var addProductIfNecessary = (req, res, callback) => {
   const body = req.body;
   console.log('post body:', body);
   const post = {
@@ -39,39 +39,6 @@ var addFarmIfNecessary = (req, res, callback1, callback2) => {
   };
   console.log('post', post, post.farmName);
 
-  connection.query({
-      text: 'SELECT EXISTS (SELECT * FROM farms WHERE farm_name = $1)',
-      values: [post.farmName]
-    }, 
-
-    (err, result) => {
-    //if the farm is not in the table, add it
-    if (!result.rows[0].exists) {
-      connection.query({
-          text: 'INSERT INTO farms (farm_name, location, phone) VALUES ($1, $2, $3)',
-          values: [post.farmName, post.farmLocation, post.farmPhone]
-        }, 
-
-        (err, result) => {
-          if (err) {
-            console.error('error:', err);
-          } else {
-            console.log('farm added!');
-            //then move on to addProductIfNecessary
-            callback1(req, res, post, callback2);
-          }
-        }
-      );
-    //if the farm is already in the table, just move on to addProductIfNecessary
-    } else {
-      console.log('farm already in table!');
-      callback1(req, res, post, callback2);
-    }
-  });
-};
-
-//add the product to the products table if it wasn't already there
-var addProductIfNecessary = (req, res, post, callback) => {
   connection.query({
       text: 'SELECT EXISTS (SELECT * FROM products WHERE product_name = $1)',
       values: [post.productName] 
@@ -96,7 +63,7 @@ var addProductIfNecessary = (req, res, post, callback) => {
           }
         }
       );
-    //if the farm is already in the table, just move on to addPost
+    //if the product is already in the table, just move on to addPost
     } else {
       console.log('product already in table!');
       callback(req, res, post);
@@ -135,7 +102,7 @@ var addPost = (req, res, post) => {
   );
 };
 
-//add the farm and the product to their respective tables is necessary, then adds the post to the posts table
+//add the product to the products table if necessary, then add the post to the posts table
 exports.handlePost = (req, res) => {
-  addFarmIfNecessary(req, res, addProductIfNecessary, addPost);
+  addProductIfNecessary(req, res, addPost);
 };
